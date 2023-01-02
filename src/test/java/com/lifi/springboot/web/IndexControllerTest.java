3230a5e881f8dd20b8ceb957666721d12b878f5a
gomljo <gomljo0307@naver.com>
2023-01-02 12:58:13 +0900

    test(IndexControllerTest): test url mapping

---
+++ src/test/java/com/lifi/springboot/web/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package com.lifi.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+        // 문자 간 띄어쓰기에 유의하기
+        // contains 함수는 약간의 변형도 다른 문자열로 인식한다.
+    }
+}
