07f0f8e6d0e2c3aaa90a9ca16a62f877bf164047
gomljo <gomljo0307@naver.com>
2023-01-06 17:44:17 +0900

    feat(PostsService): create service that find all posts

--- src/main/java/com/lifi/springboot/service/posts/PostsService.java
+++ src/main/java/com/lifi/springboot/service/posts/PostsService.java
@@ -8,6 +8,8 @@ import com.lifi.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -31,6 +33,18 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+
+    // @Transactional(readOnly = true)
+    // 트랜잭션의 범위는 유지하되, 조회 기능만 남겨두어 조회속도가 개선되기 때문에 등록, 수정, 삭제 기능이
+    // 전혀 없는 서비스 메소드에서 사용할 수 있다고 한다. - 스프링부트와 AWS로 혼자 구현하는 웹 서비스 책에서 인용 -
+
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     public  PostsResponseDto findById(Long id){
