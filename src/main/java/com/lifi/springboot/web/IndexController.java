9289f11318c110510dbfc1435a02a67bb91465a9
gomljo <gomljo0307@naver.com>
2023-01-12 17:44:42 +0900

    refactor: change method that get session value

--- src/main/java/com/lifi/springboot/web/IndexController.java
+++ src/main/java/com/lifi/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lifi.springboot.web;
 
+import com.lifi.springboot.config.auth.LoginUser;
 import com.lifi.springboot.config.auth.dto.SessionUser;
 import com.lifi.springboot.service.posts.PostsService;
 import com.lifi.springboot.web.dto.PostsResponseDto;
@@ -17,12 +18,10 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
         // postsService.findAllDesc()로 가져온 결과를 index.mustache 내의 '#posts'로 전달하는 역할
         // index.mustache를 살펴보면 반복문으로 모든 게시글을 조회하는 부분이 있으며 그곳에 #posts 변수가 있다.
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
         if (user!= null){
             model.addAttribute("userName", user.getName());
         }
