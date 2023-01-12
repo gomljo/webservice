fda5b0b4f4e9ec1a07a8fd5833663f6f5ef7354c
gomljo <gomljo0307@naver.com>
2023-01-12 17:03:43 +0900

    feat: create social login service

--- src/main/java/com/lifi/springboot/web/IndexController.java
+++ src/main/java/com/lifi/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lifi.springboot.web;
 
+import com.lifi.springboot.config.auth.dto.SessionUser;
 import com.lifi.springboot.service.posts.PostsService;
 import com.lifi.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
         // postsService.findAllDesc()로 가져온 결과를 index.mustache 내의 '#posts'로 전달하는 역할
         // index.mustache를 살펴보면 반복문으로 모든 게시글을 조회하는 부분이 있으며 그곳에 #posts 변수가 있다.
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user!= null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
