55aa827fc0469b61a289d88bb940bfaf74d04894
gomljo <gomljo0307@naver.com>
2023-02-19 15:38:18 +0900

    chore(deploy.sh): change shell script for correcting typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=webservice
 
@@ -8,12 +9,12 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID = $(pgrep -f $(PROJECT_NAME).*.jar)
+CURRENT_PID=$(pgrep -fl webservice | grep jar | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "CURRENT_PID" ]; then
-    echo ">현재 구동중인 애프릴케션이 없으므로 종료하지 않습니다."
+if [ -z "$CURRENT_PID" ]; then
+    echo ">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
@@ -23,7 +24,7 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME = $(ls - tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls - tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
